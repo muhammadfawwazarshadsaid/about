d05ffb93a705f78be87a67838ffc69081f212a17
muhammadfawwazarshadsaid <muhammad.fawwaz22@ui.ac.id>
2024-08-27 18:45:14 +0700

    lagilagi

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "942462685"
+    serviceWorkerVersion: "750744143"
   }
 });
